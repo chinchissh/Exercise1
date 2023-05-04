d7f994de809986b1f59e126c95ea0e9c84595d84
Sany-SHiriCE <Sany-SHiriCE@302D-10>
2023-05-04 15:32:32 +0700

    Добавьте файлы проекта.

---
+++ Exercise1/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    class City
+    {
+        public string Name { get; set; }
+        public GeoLocation Location { get; set; }
+    }
+}
+
---
+++ Exercise1/GeoLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    class GeoLocation
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
